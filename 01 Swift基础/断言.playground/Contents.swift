71832eff478d11ada1778359af8e7787d216889c
wx91 <766221790@qq.com>
2015-11-16 23:20:11 +0800

    swift的基本类型讲解

---
+++ 01 Swift基础/断言.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+// assertions 断言
+import UIKit
+
+var str = "Hello, playground"
+
+var 年龄=19
+
+assert(年龄>18,"必须年满18岁方可继续玩本游戏");
+
---
+++ 01 Swift基础/浮点类型.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+//可以查看浮点典型的精度Float 7位
+let PI:Float=3.123456789012345
+//Double类型为14为 默认是Double类型
+let PI2:Double=3.1234567890123456
