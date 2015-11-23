56ed4a00014d077fcf1aab94438443164ab3ab62
wx91 <766221790@qq.com>
2015-11-23 10:17:15 +0800

    下标脚本

---
+++ 下标脚本.playground/Contents.swift
@@ -0,0 +1,46 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+struct TimesTable {
+    let multiplier : Int
+    subscript(index:Int) -> Int{
+        return multiplier * index
+    }
+}
+let threeTimesTable = TimesTable(multiplier: 3)
+print("3的6倍是\(threeTimesTable[6])")
+
+
+var numberOfLegs = ["spider":8,"ant":6,"cat":4];
+numberOfLegs["brid"] = 2
+
+struct Matrix {
+    let rows:Int,columns:Int
+    var grid:[Double]
+    init(rows:Int,columns:Int){
+        self.rows = rows
+        self.columns = columns
+        grid = Array(count: rows*columns, repeatedValue: 0.0)
+    }
+    func indexIsValidForRow(row:Int,column:Int) -> Bool{
+        return row>=0&&row<rows&&column>=0&&column<columns
+    }
+    subscript(row:Int,column:Int) -> Double{
+        get{
+            assert(indexIsValidForRow(row, column: column),"Index out of range")
+            return grid[(row*columns)+column]
+        }
+        set{
+            assert(indexIsValidForRow(row, column: column),"Index out of range")
+            grid[(row*columns)+column] = newValue
+        }
+    }
+}
+var matrix = Matrix(rows: 2, columns: 2)
+matrix[0,1] = 1.5
+matrix[1,0] = 3.2
+//越界
+//let someValue = matrix[2,2]
